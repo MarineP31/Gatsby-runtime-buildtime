8df15f0a66d3c423736c2f416f5a79c13daba63a
Marine Petit <marine.petit@ekino.com>
2020-02-17 11:41:31 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
+
+  // ----------------------
+  // BUILDTIME DATA FETCHING
+  // ----------------------
+  const buildtimePosts = data.cms.blogPosts
+
+  // ----------------------
+  // RUNTIME DATA FETCHING
+  // ----------------------
+  const [starsCount, setStarsCount] = useState(0)
+  useEffect(() => {
+    // get data from GitHub api
+    fetch(`https://api.github.com/repos/gatsbyjs/gatsby`)
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        setStarsCount(resultData.stargazers_count)
+      }) // set data for the number of stars
+  }, [])
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <Bio />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+      <h1>Queries at buildtime</h1>
+      {buildtimePosts.map(posts => {
+        return (
+          <article key={posts.slug}>
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={posts.slug}>
+                  {posts.title}
+                </Link>
+              </h3>
+              <small>{posts.createdAt}</small>
+            </header>
+          </article>
+        )
+      })}
+      <h1>Queries at runtime</h1>
+      <p>Gatsby GitHub: {starsCount} stars</p>
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    cms {
+      blogPosts(where: { status: PUBLISHED }, orderBy: createdAt_DESC) {
+        title
+        createdAt
+        slug
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
